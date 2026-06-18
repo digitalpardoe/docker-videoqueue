205289ff07c94606caa44fc8b8fc597130f95134
Alex Pardoe <alex@pardoe.io>
2026-06-18 15:50:11 +0100

    Revert "Wrap array access in a mutex in case we're adding a lot of videos."
    This reverts commit 39a4d6be48cc89bc4d7b883b757491f151e98a11.

--- app.rb
+++ app.rb
@@ -9,16 +9,11 @@ puts "Running as user #{Process.uid} and group #{Process.gid}."
 Video = Struct.new('Video', :url, :downloaded, :retries)
 
 videos = []
-videos_mutex = Mutex.new
 
 Thread.new do
   puts "Starting download thread..."
   loop do
-    video = nil
-    videos_mutex.synchronize do
-      video = videos.find { |v| v.downloaded == false }
-    end
-
+    video = videos.find { |v| v.downloaded == false }
     if video.nil?
       puts "No videos to download!"
     else
@@ -26,9 +21,7 @@ Thread.new do
         result = system("yt-dlp --newline -f \"bv[height<=1080][vcodec~='^((he|a)vc|h26[45])']+ba[acodec~='^(aac|mp4a)']/bv[height<=1080]+ba/bv+ba\" -o \"/downloads/%(uploader)s/%(upload_date)s - %(title)s.%(ext)s\" \"#{video.url}\"")
         if result
           puts "Finished downloading #{video.url}!"
-          videos_mutex.synchronize do
-            video.downloaded = true
-          end
+          video.downloaded = true
         else
           handle_error(video)
         end
@@ -43,12 +36,10 @@ end
 
 def handle_error(video)
   puts "Error downloading #{video.url}!"
-  videos_mutex.synchronize do
-    video.retries += 1
-    if video.retries >= 3
-      puts "Giving up on #{video.url}!"
-      video.downloaded = true
-    end
+  video.retries += 1
+  if video.retries >= 3
+    puts "Giving up on #{video.url}!"
+    video.downloaded = true
   end
 end
 
@@ -58,8 +49,6 @@ end
 
 post '/add' do
   payload = JSON.parse(request.body.read)
-  videos_mutex.synchronize do
-    videos << Video.new(payload['url'], false, 0)
-  end
+  videos << Video.new(payload['url'], false, 0)
   status 201
 end
